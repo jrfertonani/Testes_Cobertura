bf0aa25025ed72728170e72337033d8e788d816c
jrfertonani <ademirjuniorfertonani@gmail.com>
2024-04-17 10:53:28 -0300

    endpoint delete para user

--- src/main/java/api/api/resources/UserResource.java
+++ src/main/java/api/api/resources/UserResource.java
@@ -17,13 +17,14 @@ import java.util.stream.Collectors;
 @RequestMapping("/user")
 public class UserResource {
 
+    public static final String ID = "/{id}";
     @Autowired
     private ModelMapper mapper;
 
     @Autowired
     private UserService service;
 
-    @GetMapping("/{id}")
+    @GetMapping(ID)
     public ResponseEntity<UserDTO> findById(@PathVariable Integer id) {
 
         return ResponseEntity.ok().body(mapper.map(service.findById(id),UserDTO.class));
@@ -42,18 +43,24 @@ public class UserResource {
     @PostMapping
     public ResponseEntity<UserDTO> create(@RequestBody UserDTO obj){
         URI uri = ServletUriComponentsBuilder
-                .fromCurrentRequest().path("/{id}")
+                .fromCurrentRequest().path(ID)
                 .buildAndExpand(service.create(obj))
                 .toUri();
         return ResponseEntity.created(uri).build();
     }
 
-    @PutMapping("/{id}")
+    @PutMapping(ID)
     public ResponseEntity<UserDTO> update(@PathVariable Integer id,
                                           @RequestBody UserDTO obj){
         obj.setId(id);
         return ResponseEntity.ok().body(mapper.map(service.update(obj), UserDTO.class));
 
+    }
+
+    @DeleteMapping(ID)
+    public ResponseEntity<UserDTO> delete(@PathVariable Integer id){
+        service.delete(id);
+        return ResponseEntity.noContent().build();
     }
 
 }
