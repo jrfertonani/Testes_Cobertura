87c80529ddf2a947236cac1941d3441f9e9160fa
jrfertonani <ademirjuniorfertonani@gmail.com>
2024-04-16 21:46:30 -0300

    FindyAll User

--- src/main/java/api/api/services/UserService.java
+++ src/main/java/api/api/services/UserService.java
@@ -3,7 +3,11 @@ package api.api.services;
 
 import api.api.domain.User;
 
+import java.util.List;
+
 public interface UserService {
 
     User findById(Integer id);
+
+    List<User> findAll();
 }
--- src/main/java/api/api/services/impl/UserServiceImpl.java
+++ src/main/java/api/api/services/impl/UserServiceImpl.java
@@ -8,6 +8,7 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -16,15 +17,18 @@ public class UserServiceImpl implements UserService {
 
 
     @Autowired
-    private UserRepository Repository;
+    private UserRepository repository;
 
     @Override
     public User findById(Integer id) {
-        Optional<User> obj = Repository.findById(id);
+        Optional<User> obj = repository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! id: " +id));
     }
 
-
+    @Override
+    public List<User> findAll() {
+        return repository.findAll();
+    }
 
 
 }
